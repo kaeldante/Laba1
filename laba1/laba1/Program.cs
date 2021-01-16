9c788633cfb240f70c4e0efbdebec6545627adb1
Danil Yavorskiy <63715687+kaeldante@users.noreply.github.com>
2021-01-16 10:52:40 +0600

    Add files via upload

---
+++ laba1/laba1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: 123
+ * Дата: 16.01.2021
+ * Время: 10:44
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace laba1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.Write("Введите имя пользователя: " );
+			string name = Console.ReadLine();
+			Console.WriteLine("Добро пожаловать: " + name);
+			Console.WriteLine("Для выхода нажмите любую клавишу!");
+			
+			Console.ReadKey();
+			
+		}
+	}
+}
